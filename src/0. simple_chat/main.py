e71e269402e3c1f8dfddfe46f26b19ca02fdc5d2
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-10-01 22:58:06 +0200

    src/0 upd

--- src/0. simple_chat/main.py
+++ src/0. simple_chat/main.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
     calls = AnthropicCalls(api_key=ANTHROPIC_API_KEY, stream=True)
 
+    n = 50
     message = ""
     while message != "END": 
         message = input("User:")
-        calls.chat(message=message)
+        calls.chat(message=f"{message} please use {n} words or less")
